901d2e59ff21a170373edb80afab09d723025122
that devguy <zkmutch@gmail.com>
2024-01-10 17:52:28 -0500

    built out the HangmanWord component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,30 @@
 export function HangmanWord() {
-    
+  const word = "test";
+  const guessedLetters = ["t"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
